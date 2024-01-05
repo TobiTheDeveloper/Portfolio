a6eb1c4db143c4a5876e129715e4d9db9676f06b
TobiTheDeveloper <yusuffoyediranyo@gmail.com>
2024-01-04 19:19:24 -0500

    commit

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // No JavaScript Needed. Thank God!!!
 
 //JavaScript to open resume
-
 function openResume() {
     window.open('./Yusuff Oyediran Resume.pdf', '_blank');
+}
+
+//JavaScript to open Certificates and Degree
+function openCertificates() {
+    window.open(); //I will add all my certificate into one as PDF
 }
